d529effec676313872b6d10556a2581221c7364b
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-02-21 22:32:09 +0300

    HomeWorkpictures

---
+++ Pictures.cs
@@ -0,0 +1,7 @@
+﻿int pictures = 52;
+int rowCapacity = 3;
+
+int rowIsFilled = pictures / rowCapacity;
+int picturesNotInRow = 52 % 3;
+Console.WriteLine($"заполненныйх рядов с картинками: {rowIsFilled} , картинок осталось: {picturesNotInRow}");
+Console.ReadKey();
