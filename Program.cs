3ca190618b56d1070e64ce043ad6593d07cb2e1e
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-02-21 22:16:25 +0300

    String

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,28 +11,32 @@ namespace JaJunior
     {
         static void Main(string[] args)
         {
-            int weightMan = 0;
-            int weightWoman = 0;
-            float haightMan = 0f;
-            float haightWoman = 0f;
-            string greeting = "Привет! Пожалуйста, укажите ваш вест и рост, а также вес и рост вашего партнёра";
-            bool isCompatible = true;
-            int skinny = 19;
-            int normal = 25;
-            int fatty = 30;
-            int percent = 100;
-            double manStats = weightMan / (haightMan / percent );
-            double womanStats = weightWoman / (haightWoman / percent );
-            int statusMan;
-            int statusWoman;
-            bool isTrust = true;
-            char manLetter = 'М';
-            char womanLetter = 'Ж';
-
+            string greeating = "Вас приветсвует Система, для загрузки Вашего разума на наши сервера, пожалуйста, ответьте на следующие вопросы:";
+            string firstQuestion = "Напишете, пожалуйста, Ваше имя: ";
+            string secondQuestion = "Сколько Вам лет: ";
+            string firdQuestion = "Какой Ваш знак зодиака: ";
+            string fourthQuestion = "Где Вы работали до того как приняли решение стать чатью VR вселенной: ";
+            string fifthQuestion = "Если вы согласны стать частью системы напишите: ДА ";
+            string name;
+            int age;
+            string zodiac;
+            string workPlace;
+            Console.WriteLine(greeating);
+            Console.Write(firstQuestion);
+            name = Console.ReadLine();
+            Console.Write(secondQuestion);
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write(firdQuestion);
+            zodiac = Console.ReadLine();
+            Console.Write(fourthQuestion);
+            workPlace = Console.ReadLine();
+            Console.WriteLine(fifthQuestion);
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine($"Поздравляю Вас, {name}, Вы прожили такую долгую жизнь {age}г. Вас всегда вела звезда {zodiac}. " +
+                $"Возможно Вас сильно ценили пока вы работали в {workPlace}. Но теперь вы собственность системы...");
 
         }
-
-
 
     }
 }
