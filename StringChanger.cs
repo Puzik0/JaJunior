c318c2674f0e46c115d43c9c07291652772e1369
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-02-21 22:41:40 +0300

    stringChanger

---
+++ StringChanger.cs
@@ -0,0 +1,10 @@
+﻿string name = "Скучнов";
+string secondname = "Георгий";
+
+Console.WriteLine($"имя: {name}, фамилия {secondname}");
+
+string clipboard = name; //не уверен можно ли это сделать без буфера
+name = secondname;
+secondname = clipboard;
+
+Console.WriteLine($"имя: {name}, фамилия {secondname}");
