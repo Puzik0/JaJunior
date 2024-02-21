f196f987e21f82859d48ee1dc91b7d96590ef786
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-02-21 23:38:42 +0300

    Queue

---
+++ Queue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTimeTreker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeForOnePerson = 10;
+            int PersonInFront;
+            int allMinutes;
+            int minutesInHour = 60;
+
+            int hours;
+            int minutes;
+
+            Console.WriteLine("Сколько сейчас человек перд Вами?");
+            PersonInFront = Convert.ToInt32(Console.ReadLine());
+
+            allMinutes = PersonInFront * timeForOnePerson;
+            hours = allMinutes / minutesInHour;
+            minutes = allMinutes % minutesInHour;
+
+            Console.WriteLine($"Вам тут еще тарчать {hours} часов и {minutes} минут");
+            Console.ReadKey(); 
+
+        }
+    }
+}
