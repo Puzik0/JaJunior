b12b204e4cc97624e5f7f8ea4c824b0430b29060
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-02-25 22:06:19 +0300

    cycleBegin

---
+++ CycleBegin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclesBegin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int retryTimes;
+
+            Console.Write("Что ты хочешь сказать? ");
+            userInput = Console.ReadLine();
+            Console.Write("Сколько раз повторить? ");
+            retryTimes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = retryTimes; i > 0; i--)
+            {
+                Console.WriteLine(userInput);
+            }
+        }
+    }
+}
