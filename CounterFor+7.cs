2be09cbef43f3286d0218b1402d7f7af8fe2520a
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-02-25 23:21:01 +0300

    counters

---
+++ CounterFor+7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 5;
+            int maxNumber = 103;
+            int stepNumber = 7;
+            int steps = (maxNumber - minNumber) / stepNumber;
+            int result = minNumber;
+
+            for (int i = 0; i <= steps ; i++)
+            {
+                Console.WriteLine(result);
+                result += stepNumber;
+            }
+        }
+    }
+}
---
+++ CounterWhile+7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterWhile_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 5;
+            int maxNumber = 103;
+            int stepNumber = 7;
+            
+            while (minNumber <= maxNumber)
+            {
+                Console.WriteLine(minNumber);
+                minNumber += stepNumber;
+            }
+        }
+    }
+}
