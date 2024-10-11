5d3e2f8864160efd4cc94e7a4a0454eb35cfa4b9
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-11 16:39:14 +0300

    MultiplesOfNumbers

---
+++ MultiplesOfNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MultiplesOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minNumberForRandom = 10;
+            int maxNumberForRandom = 25;
+            int randomNumberForCount = random.Next(minNumberForRandom, maxNumberForRandom);
+
+            int minCountSpace = 50;
+            int maxCountSpace = 150;
+            int counterOfNumbersInclude = 0;
+
+            for (int i = minCountSpace; i <= maxCountSpace; i += randomNumberForCount)
+            {
+                counterOfNumbersInclude++;
+            }
+
+            Console.WriteLine($" Число {randomNumberForCount}, кол-во кратных чисел {counterOfNumbersInclude} \n в промежутке между {minCountSpace} и {maxCountSpace}");
+        }
+    }
+}
