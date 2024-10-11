58cb348df8b4d69b12aef88e6ac074ef08d04019
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-11 17:40:38 +0300

    sad

---
+++ DegreeOfTwo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DegreeOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int RequestedNumber = 2;
+
+            int counterDegree = 0;
+            int numberInDegree = 2;
+
+            Random random = new Random();
+            int minNumberForRandom = 1;
+            int maxNumberForRandom = 100;
+            int randomNumberForCount = random.Next(minNumberForRandom, maxNumberForRandom);
+
+            for (int i = RequestedNumber; i <= randomNumberForCount; i *= RequestedNumber)
+            {
+                counterDegree++;
+                numberInDegree = i;
+            }
+
+            if (randomNumberForCount >= numberInDegree)
+            {
+                counterDegree++;
+                numberInDegree *= RequestedNumber;
+            }
+
+            Console.WriteLine($"число = {randomNumberForCount},\nстепень = {counterDegree}\nдвойка в данной степени = {numberInDegree} ");
+        }
+    }
+}
