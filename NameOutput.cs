94797e34b40907188b839f9e60eb94dfb26f92ed
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-11 15:40:50 +0300

    Create NameOutput.cs

---
+++ NameOutput.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthUserName;
+
+            string outputSymbolString ="";
+            string userName;
+            string specialSymbol;
+
+            Console.WriteLine("Введите Ваше имя");
+
+            userName = (Console.ReadLine());
+
+            Console.WriteLine("Введите специальный символ");
+
+            specialSymbol = (Console.ReadLine());
+
+            lengthUserName = userName.Length;
+
+            for (int i = 0; i < lengthUserName; i++)
+            {
+                outputSymbolString += specialSymbol;
+            }
+
+            Console.WriteLine($"{outputSymbolString} \n{userName} \n{outputSymbolString}");
+        }
+    }
+}
