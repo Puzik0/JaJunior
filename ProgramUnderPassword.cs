3a9b9822456b4ace08b4f9defa523e32c23b657d
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-11 15:59:53 +0300

    ProgramUnderPasswordCreated

---
+++ ProgramUnderPassword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramUnderPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Password = "HARDpassword";
+
+            int attemptsPasswordInput = 3;
+
+            string userInput;
+
+            for (int i = 0; i < attemptsPasswordInput; i++)
+            {
+                Console.WriteLine("Введите пароль");
+                userInput = Console.ReadLine();
+
+                if (userInput == Password)
+                {
+                    Console.WriteLine("Вы успешно вошли в программу");
+                    break;
+                }
+            }
+        }
+    }
+}
