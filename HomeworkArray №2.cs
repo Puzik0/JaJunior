87b93bd73f315bd2fe8db027280a7f3253d2606f
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-17 01:07:59 +0300

    Update HomeworkArray №2.cs

--- HomeworkArray №2.cs
+++ HomeworkArray №2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace HomeworkArray__2
 {
@@ -7,22 +7,23 @@ namespace HomeworkArray__2
         static void Main(string[] args)
         {
             int maxNumberInArray = int.MinValue;
+            int requestedValue = 0;
 
-            int[,] A =
+            int[,] arrey =
             {
-                { 1, 2, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 9, 1, 1, 1 },
-                { 1, 1, 1, 1, 7, 1, 1, 1, 1 },
-                { 1, 1, 1, 5, 1, 1, 1, 1, 1 },
+                { 1, 122, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 122, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 117, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 122, 1, 1, 1 },
-                { 1, 1, 1, 13, 1, 1, 1, 1, 1 },
+                { 1, 1, 122, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 122, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 122 },
             };
 
-            foreach (int item in A)
+            foreach (int item in arrey)
             {
                 if (maxNumberInArray < item)
                 {
@@ -30,11 +31,11 @@ namespace HomeworkArray__2
                 }
             }
 
-            for (int i = 0; i < A.GetLength(0); i++)
+            for (int i = 0; i < arrey.GetLength(0); i++)
             {
-                for (int j = 0; j < A.GetLength(1); j++)
+                for (int j = 0; j < arrey.GetLength(1); j++)
                 {
-                    Console.Write(A[i, j] + " ");
+                    Console.Write(arrey[i, j] + " ");
                 }
 
                 Console.WriteLine();
@@ -42,23 +43,23 @@ namespace HomeworkArray__2
 
             Console.WriteLine($"\nИсходная матрица и максимально число в ней : {maxNumberInArray}\n");
 
-            for (int i = 0; i < A.GetLength(0); i++)
+            for (int i = 0; i < arrey.GetLength(0); i++)
             {
-                for (int j = 0; j < A.GetLength(1); j++)
+                for (int j = 0; j < arrey.GetLength(1); j++)
                 {
-                    if (A[i, j] == maxNumberInArray)
+                    if (arrey[i, j] == maxNumberInArray)
                     {
-                        A[i, j] = 0;
+                        arrey[i, j] = requestedValue;
                         break;
                     }
                 }
             }
 
-            for (int i = 0; i < A.GetLength(0); i++)
+            for (int i = 0; i < arrey.GetLength(0); i++)
             {
-                for (int j = 0; j < A.GetLength(1); j++)
+                for (int j = 0; j < arrey.GetLength(1); j++)
                 {
-                    Console.Write(A[i, j] + " ");
+                    Console.Write(arrey[i, j] + " ");
                 }
 
                 Console.WriteLine();
