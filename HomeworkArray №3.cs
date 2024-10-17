b8da491cd48935dd6404fef3d2fd6a6da441b49a
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-17 12:20:14 +0300

    2

---
+++ HomeworkArray №3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkArray__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 9, 51, 51, 65, 1, 61, 51, 561, 51, 65, 651, 561, 684, 84, 654, 645, 46, 84, 321, 3, 5 };
+
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == 0 || i == array.Length - 1)
+                {
+                    if (i == 0 && array[i] > array[i + 1])
+                    {
+                        Console.WriteLine($"\nЛокальное максимально значение {array[i]}");
+                    }
+                    else if (i == array.Length - 1 && array[i] > array[i - 1])
+                    {
+                        Console.WriteLine($"\nЛокальное максимально значение {array[i]}");
+                    }
+                }
+                else if (array[i - 1] < array[i] && array[i] > array[i + 1])
+                {
+                    Console.WriteLine($"\nЛокальное максимально значение {array[i]}");
+                }
+            }
+        }
+    }
+}
