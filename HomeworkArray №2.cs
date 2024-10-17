639e767b0dc3c67a3ca01f1c3c161f2d9cfdbe57
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-17 12:16:21 +0300

    Update HomeworkArray №2.cs

--- HomeworkArray №2.cs
+++ HomeworkArray №2.cs
@@ -9,21 +9,21 @@ namespace HomeworkArray__2
             int maxNumberInArray = int.MinValue;
             int requestedValue = 0;
 
-            int[,] arrey =
+            int[,] numbers =
             {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 122, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 122, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 122, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 122, 1, 1, 1, 1, 1 },
+                { 1, 1, 122, 122, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 122 },
             };
 
-            foreach (int item in arrey)
+            foreach (int item in numbers)
             {
                 if (maxNumberInArray < item)
                 {
@@ -31,11 +31,11 @@ namespace HomeworkArray__2
                 }
             }
 
-            for (int i = 0; i < arrey.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                for (int j = 0; j < arrey.GetLength(1); j++)
+                for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    Console.Write(arrey[i, j] + " ");
+                    Console.Write(numbers[i, j] + " ");
                 }
 
                 Console.WriteLine();
@@ -43,23 +43,22 @@ namespace HomeworkArray__2
 
             Console.WriteLine($"\nИсходная матрица и максимально число в ней : {maxNumberInArray}\n");
 
-            for (int i = 0; i < arrey.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                for (int j = 0; j < arrey.GetLength(1); j++)
+                for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    if (arrey[i, j] == maxNumberInArray)
+                    if (numbers[i, j] == maxNumberInArray)
                     {
-                        arrey[i, j] = requestedValue;
-                        break;
+                        numbers[i, j] = requestedValue;
                     }
                 }
             }
 
-            for (int i = 0; i < arrey.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                for (int j = 0; j < arrey.GetLength(1); j++)
+                for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    Console.Write(arrey[i, j] + " ");
+                    Console.Write(numbers[i, j] + " ");
                 }
 
                 Console.WriteLine();
