b6966357cd2d186cb8ce2e970e4d3333c107c584
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-20 20:18:30 +0300

    2

---
+++ HomeworkArrayN6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkArrayN6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int tempNumber = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = tempNumber;
+                    }
+                }
+            }
+
+            foreach (int item in numbers) { Console.Write(item + " "); }
+        }
+    }
+}
