f3c9c15d09dd48291c673c74883300ed7a9f24d4
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-20 19:30:35 +0300

    homework5

---
+++ HomeworkArrayN5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HomeworkArrayN5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 1, 2, 3, 5, 4, 5, 8, 5, 5, 5, 6, 6, 6, 6, 6, 6, 2, 2, 2, 3, 2, 1, 1, 1, 4, 4, 4, 8, 9, 9, 9, 9, 6, 3 };
+            int counter = 1;
+            int counterTemp = 0;
+            int number = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    counter++;                    
+                }
+
+                else                                    
+                {
+                    if (counter > counterTemp)         
+                    {
+                        counterTemp = counter;           
+                        number = numbers[i - 1];
+                    }
+
+                    counter = 1;                             
+                }
+            }
+
+            Console.WriteLine($"Число:{number}, кол-во повторений: {counterTemp} ");
+        }
+    }
+}
