1e5a4d7c8eb4040cb76100a19a3dc4f1725f6dcb
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-20 20:29:09 +0300

    7

---
+++ HomeworkArrayN7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeworkArrayN7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForSplit = "Тут должны быть какие-то слова, дабы программа их разделила?";
+            string[] someStrings = stringForSplit.Split(' ');
+
+            foreach (string str in someStrings) { Console.WriteLine(str); }
+
+        }
+    }
+}
