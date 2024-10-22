e54a30099934ca020b45a0e8798aa7574ae6f954
Puzik0 <135577653+Puzik0@users.noreply.github.com>
2024-10-22 20:30:47 +0300

    HWKansasCityShuffle

---
+++ HomeworkMetods/HWKansasCityShuffle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HWKansasCityShuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            WriteArray(numbers);
+            Console.WriteLine();
+            ShuffleArray(numbers);
+            WriteArray(numbers);
+
+        }
+
+        static void WriteArray(int[] array)
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        static void ShuffleArray(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomIndex = random.Next(0, array.Length);
+                int tempNumber = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = tempNumber;
+            }
+        }
+    }
+}
